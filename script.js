1369fc36b32980f4e80696e616878ebb3de47213
bluefortythree <117217594+bluefortythree@users.noreply.github.com>
2023-03-23 14:37:30 -0700

    add functionality for buttons

--- script.js
+++ script.js
@@ -12,19 +12,45 @@ new Book('Me', 'How I learned to Code', '150', 'no')
 new Book('You', 'Captain Underpants', '200', 'no')
 new Book('J.K. Rowling', 'Harry Potter and the Sorcerer\'s Stone', '900', 'yes')
 
-let button = document.querySelector('#add');
-button.addEventListener('click', () => {
-  const books = document.getElementById('books');
-  const newBook = document.createElement('div');
-  newBook.innerText = 'book';
-  books.appendChild(newBook);
-  })
+// let button = document.querySelector('#add');
+// button.addEventListener('click', () => {
+//   const books = document.getElementById('books');
+//   const newBook = document.createElement('div');
+//   newBook.innerText = 'book';
+//   books.appendChild(newBook);
+//   })
 
 // for (let i = 0; i < myLibrary.length; i++) {
 //   const div = document.createElement('div');
 //   document.body.appendChild(div)
 //   div.innerText = 'hello world'  
 // }
+
+let button = document.querySelector('#add');
+button.addEventListener('click', () => {
+  const form = document.getElementById('pop-up');
+  form.style.display = 'block';
+})
+
+let cancel = document.querySelector('#cancel');
+cancel.addEventListener('click', () => {
+  const clear = document.getElementById('form');
+  clear.reset();
+  const form = document.getElementById('pop-up');
+  form.style.display = "none";
+})
+
+let submit = document.querySelector('#confirm');
+submit.addEventListener('click', () => {
+  const books = document.getElementById('books');
+  const newBook = document.createElement('div');
+  newBook.innerText = 'book';
+  books.appendChild(newBook);
+  const clear = document.getElementById('form');
+  clear.reset();
+  const form = document.getElementById('pop-up');
+  form.style.display = 'none';
+})
 
 function addBookToLibrary() {
   // do stuff here
